6578ddca936c29162f41feb56b1a96c6d0c0b50f
bennett <2165217440@qq.com>
2026-06-16 20:31:38 +0800

    refactor: revert dynamic loading, switch back to static linking and add bindgen allowlist

--- crates/titanengine-sys/build.rs
+++ crates/titanengine-sys/build.rs
@@ -10,13 +10,20 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let root_dir = PathBuf::from(&manifest_dir).parent().unwrap().parent().unwrap().to_path_buf();
     
-    // We do NOT link against the .lib file anymore!
-    // Instead we use bindgen's dynamic library feature to load TitanEngine.dll at runtime.
+    // Link search path
+    println!("cargo:rustc-link-search=native={}", root_dir.display());
+    
+    let target = env::var("TARGET").unwrap();
+    if target.contains("x86_64") {
+        println!("cargo:rustc-link-lib=dylib=TitanEngine");
+    } else {
+        println!("cargo:rustc-link-lib=dylib=TitanEngine");
+    }
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg("-xc++")
-        .dynamic_library_name("TitanEngine")
+        .allowlist_file(".*TitanEngine\\.h")
         .generate()
         .expect("Unable to generate bindings");
 
